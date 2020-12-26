f57dc338a02d81ed8b821228b69c9463c1de90e1
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2020-12-26 09:35:30 -0600

    added ui feature to parene element of draggable features

--- assets/js/script.js
+++ assets/js/script.js
@@ -188,7 +188,7 @@ var dropZoneDragHandler = function(event){
   var taskListEl = event.target.closest(".task-list");
   if(taskListEl){
     event.preventDefault();
-    
+    taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
   }
 };
 
@@ -210,8 +210,17 @@ var dropTaskHandler = function(event) {
     statusSelectEl.selectedIndex = 2;
   }
 
+  dropZoneEl.removeAttribute("style");
+
   dropZoneEl.appendChild(draggableElement);
 };
+
+var dragLeaveHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    taskListEl.removeAttribute("style");
+  }
+}
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
@@ -227,3 +236,5 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
